7bdafcb949e02a7e6f0ee2b3739cb9ad95c9e512
MAZIN AHMED <mazinahmedbafatia.coderkube@gmail.com>
2026-03-10 14:27:18 +0530

    Add initial migrations

---
+++ database/migrations/2026_03_10_084423_create_events_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('organization_id')
+                  ->constrained()
+                  ->cascadeOnDelete();
+
+            $table->string('title');
+            $table->text('description');
+
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+
+            $table->string('status');
+
+            $table->foreignId('created_by')
+                  ->constrained('users')
+                  ->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
